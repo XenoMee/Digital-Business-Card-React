81592f57f6ad5d806bca3377ff743ce3d8ab90f7
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-25 18:50:02 +0300

    create user interests component

---
+++ src/components/Interests.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Interests = () => (
+  <div className='card-interests'>
+    <h2 className='interests-title'></h2>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum dolor cumque
+      ducimus blanditiis quas debitis voluptate quibusdam rem corporis quaerat
+      vero impedit repudiandae, aspernatur nisi soluta unde, dolores fugit
+      repellendus.
+    </p>
+  </div>
+);
+
+export default Interests;
