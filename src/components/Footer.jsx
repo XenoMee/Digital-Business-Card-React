7b79ab09cd6ef64e555ec03a1f55b7ccf444b7fd
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-25 18:50:23 +0300

    create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className='footer'>
+    <ul className='social-list'>
+      <li className='social-item'>
+        <a className='social-link' href=''>
+          <i className='fa-brands fa-square-x-twitter'></i>
+        </a>
+      </li>
+      <li className='social-item'>
+        <a className='social-link' href=''>
+          <i className='fa-brands fa-facebook'></i>
+        </a>
+      </li>
+      <li className='social-item'>
+        <a className='social-link' href=''>
+          <i className='fa-brands fa-square-instagram'></i>
+        </a>
+      </li>
+      <li className='social-item'>
+        <a className='social-link' href=''>
+          <i className='fa-brands fa-square-github'></i>
+        </a>
+      </li>
+    </ul>
+  </footer>
+);
+
+export default Footer;
