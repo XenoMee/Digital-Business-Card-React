d6aa7242b7fe2fd25d094e0b0c27a28c59b91f36
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-25 18:49:50 +0300

    create user about component

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => (
+  <div className='card-about'>
+    <h2 className='about-title'>About</h2>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
+      tempore odio, vero totam quod ducimus veniam quisquam molestias amet
+      minima dolorem et hic voluptate consectetur maxime repellat distinctio
+      quibusdam laborum?
+    </p>
+  </div>
+);
+
+export default About;
