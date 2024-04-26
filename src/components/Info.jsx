855d44565e3dce628f91efff57bee169fdefb6a0
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-26 12:16:54 +0300

    add utility classes to parent elements

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,33 +1,30 @@
 import React from 'react';
-import profilePicture from '../images/profile-picture.png';
 
 const Info = () => (
-  <div className='card-info'>
-    <img
-      className='user-profile-picture'
-      src={profilePicture}
-      alt='User profile picture'
-    />
+  <div className='card-info grid-flow' data-spacing='medium'>
     <div className='user-info'>
       <h1 className='user-name'>Adrian Trandafir</h1>
       <span className='user-job'>Web Developer</span>
-      <a href='https://xenomee.github.io/adriantrandafir/'>
+      <a
+        className='user-website'
+        href='https://xenomee.github.io/adriantrandafir/'
+      >
         <p>Personal Website</p>
       </a>
     </div>
-    <div className='contact-info'>
+    <div className='contact-info flex-group'>
       <a
         className='btn email-btn flex-group'
         href='adrian-stefan.trandafir@outlook.com'
       >
-        <i className='fa-solid fa-envelope'></i>
+        <i className='icon fa-solid fa-envelope'></i>
         Email
       </a>
       <a
         className='btn linkedin-btn flex-group'
         href='https://www.linkedin.com/in/adriantrandafir09/'
       >
-        <i className='fa-brands fa-linkedin'></i>
+        <i className='icon fa-brands fa-linkedin'></i>
         LinkedIn
       </a>
     </div>
